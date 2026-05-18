fa90fbb17943828f6710c147a22a07ddd1cb7461
Naga Swaroof Pagolu <163762386+nagaswaroofpagolu@users.noreply.github.com>
2026-05-18 11:14:15 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/ems/ems_backend/config/CorsConfig.java
+++ backend/src/main/java/com/ems/ems_backend/config/CorsConfig.java
@@ -6,11 +6,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173,https://employee-management-system-ten-peach.vercel.app/login")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://employee-management-system-ten-peach.vercel.app"
+                )
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
