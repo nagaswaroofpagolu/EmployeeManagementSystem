d19f0386d3b20497c6167ceb53738a36cd5067d1
Naga Swaroof Pagolu <163762386+nagaswaroofpagolu@users.noreply.github.com>
2026-05-18 11:00:31 +0530

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://employeemanagementsystem-gfz3.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Add a request interceptor to attach the JWT token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
